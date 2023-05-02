1674a982de5eb10e96d423d6d09d13c74474b681
Pavel <web.js.ezh@gmail.com>
2023-05-02 13:37:32 +0300

    Добавляет карту

---
+++ source/script.js
@@ -0,0 +1,14 @@
+let navPage = document.querySelector('.page-nav');
+let navToggle = document.querySelector('.page-nav__toggle');
+
+navPage.classList.remove('page-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navPage.classList.contains('page-nav--closed')) {
+    navPage.classList.remove('page-nav--closed');
+    navPage.classList.add('page-nav--opened');
+  } else {
+    navPage.classList.add('page-nav--closed');
+    navPage.classList.remove('page-nav--opened');
+  }
+});
