87ae7b393b5fe998c17436030f215252ddd2464a
Pavel <web.js.ezh@gmail.com>
2023-05-29 23:43:46 +0300

    Добавляет исправления после первой проверки

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
 let navPage = document.querySelector('.page-nav__wrapper');
 let navToggle = document.querySelector('.main-header__toggle');
 
-navPage.classList.remove('page-nav--nojs');
+navPage.classList.remove('page-nav__wrapper--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navPage.classList.contains('page-nav--closed')) {
-    navPage.classList.remove('page-nav--closed');
-    navPage.classList.add('page-nav--opened');
+  if (navPage.classList.contains('page-nav__wrapper--closed')) {
+    navPage.classList.remove('page-nav__wrapper--closed');
+    navPage.classList.add('page-nav__wrapper--opened');
   } else {
-    navPage.classList.add('page-nav--closed');
-    navPage.classList.remove('page-nav--opened');
+    navPage.classList.add('page-nav__wrapper--closed');
+    navPage.classList.remove('page-nav__wrapper--opened');
   }
 });
